507ecae4f0ebfd9a5a7d2c0c3342fe4c287955dd
Harold_Mesa <harold.mesa@icloud.com>
2022-10-07 12:32:31 -0500

    Add Stylist.cs and Client.cs models

--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -1,8 +1,16 @@
-namespace HairSalon
+using System.Collections.Generic;
+
+namespace HairSalon.Models
 {
   public class Stylist
   {
+    public Stylist()
+    {
+      this.Clients = new HashSet<Client>();
+    }
+
     public int StylistId { get; set; }
     public string StylistSpecialty { get; set; }
+    public virtual ICollection<Client> Clients { get; set; }
   }
 }

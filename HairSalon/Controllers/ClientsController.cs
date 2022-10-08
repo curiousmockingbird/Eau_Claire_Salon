194152a357de0ef524a2e4001be93176eda043a6
Harold_Mesa <harold.mesa@icloud.com>
2022-10-08 11:56:46 -0500

    WIP: Working on Bug adding a new client to a stylist.

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -16,7 +16,7 @@ namespace HairSalon.Controllers
     }
     public ActionResult Index()
     {
-      List<Client> model = _db.Clients.ToList();
+      List<Client> model = _db.Clients.Include(client => client.Stylist).ToList();      
       return View(model);
     }
     public ActionResult Create()
@@ -24,5 +24,20 @@ namespace HairSalon.Controllers
       ViewBag.StylistId = new SelectList(_db.Stylists, "StylistId", "Name");
       return View();
     }
+
+    [HttpPost]
+    public ActionResult Create(Client client)
+    {
+      _db.Clients.Add(client);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      Client thisClient = _db.Clients.FirstOrDefault(client => client.ClientId == id);
+      return View(thisClient);
+    }
+
   }
 }
--- HairSalon/Controllers/HomeController.cs
+++ HairSalon/Controllers/HomeController.cs
@@ -7,6 +7,7 @@ namespace HairSalon.Controllers
     [HttpGet("/")]
     public ActionResult Index()
     {
+      ViewBag.PageTitle = "View all the items please";
       return View();
     }
   }
